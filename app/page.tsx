ec7d93ca18b15a21616b1a0bdda1b8fdaa29e67d
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-05-29 23:51:48 +0530

    Implement ESlint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1 className='text-3xl text-violet-700 font-black'>Welcome to Next.Js 👋</h1>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Next.Js 👋
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
