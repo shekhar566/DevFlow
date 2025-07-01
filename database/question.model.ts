9d06631e6607411e88094846cd83402d1510f83c
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-07-01 19:02:56 +0530

    Implement Question Model

---
+++ database/question.model.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  title: string;
+  content: string;
+  tags: string;
+  views: number;
+  answers: number;
+  upvotes: number;
+  downvotes: number;
+  author: Types.ObjectId;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, require: true },
+    content: { type: String, require: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    author: { type: Schema.Types.ObjectId, ref: "", require: true },
+  },
+  { timestamps: true }
+);
+
+const Question = models?.Account || model<IQuestion>("Account", QuestionSchema);
+
+export default Question;
