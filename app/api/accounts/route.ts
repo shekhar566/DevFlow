605b6c91d8b07ec62a3357f8721b6f184b8869b0
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-07-10 12:14:32 +0530

    Implement Create & Read Account Routes

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,46 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbConnect();
+
+    const account = await Account.find();
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponce;
+  }
+}
+
+// Create User
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccount: validatedData.providerAccountId,
+    });
+    if (existingAccount)
+      throw new ForbiddenError(
+        "An account with the same provider already exists"
+      );
+
+    const newAccount = await Account.create(validatedData);
+
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponce;
+  }
+}
