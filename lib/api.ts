f69a0bb10b233ad4fd7ac3da4fcb0291eee7c36c
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-08-27 17:34:15 +0530

    Added project files with images

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,6 @@ import { IUser } from "@/database/user.model";
 
 import { fetchHandler } from "./handlers/fetch";
 import ROUTES from "@/constants/routes";
-import { getAnswer } from "./actions/answer.action";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
