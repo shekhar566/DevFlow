8bbb7ce3ef77ac31568f4bf707c7789fe5a7cff8
Shekhar Anand Chaudhary <shekharchaudhary870950@gmail.com>
2025-11-28 19:56:12 +0530

    Implement Jobs Feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
@@ -20,12 +19,72 @@ const spaceGrotesk = localFont({
   weight: "300 400 500 700",
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Dev Overflow | Home",
   description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    "Dev Overflow is a community-driven platform to ask and answer real-world programming questions. Learn, grow, and connect with developers around the world.",
+
+  generator: "Next.js",
+  applicationName: "Dev Overflow",
+  referrer: "origin-when-cross-origin",
+
+  keywords: [
+    "Dev Overflow",
+    "programming questions",
+    "developer Q&A",
+    "web development",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "algorithms",
+    "data structures",
+    "developer community",
+  ],
+
+  authors: [{ name: "Shekhar" }, { name: "Dev Overflow", url: "" }],
+  creator: "Shekhar",
+  publisher: "Dev Overflow",
+
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
   icons: {
-    icon: "/images/site-logo.svg",
+    icon: "/images/site-logo.svg", // regular favicon
+    shortcut: "/favicon.ico", // browser address bar icon
+    apple: "/apple-touch-icon.png", // Apple devices
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+  },
+
+  // Optional: Theme color for browser UI and mobile experience
+  themeColor: "#18181b",
+
+  // Optional: Color for Microsoft tiles and pinned sites
+  msapplication: {
+    TileColor: "#ffffff",
+    TileImage: "/mstile-150x150.png",
   },
 };
 
